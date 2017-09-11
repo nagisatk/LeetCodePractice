5bd36517c5d0c60f2517b1ae76bafacf7a323477
Nagisa <nagisa_tk@outlook.com>
2017-09-11 22:36:51 +0800

    resolved problem

---
+++ leetcode_617.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None:
+            if t2 is None:
+                return None
+            else:
+                return t2
+        else:
+            if t2 is None:
+                return t1
+        root = TreeNode(t1.val + t2.val)
+        root.left = self.mergeTrees(t1.left, t2.left)
+        root.right = self.mergeTrees(t1.right, t2.right)
+        return root
